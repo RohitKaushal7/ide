6cd12740e89f87936fc5ac580b0fb797b243abec
RohitKaushal7 <rohitkk252550@gmail.com>
2020-03-14 09:25:06 +0530

    API

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
@@ -92,5 +102,5 @@ app.post("/python", (req, res) => {
   // console.log(req.body);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 5000);
 console.log("server at : localhost:3000");
